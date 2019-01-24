69fb61e50966ecc28fc8bca547a20f32cf24ac83
gonca <gonca@DESKTOP-UT10VI0>
2019-01-24 17:55:45 +0000

    Kata 7 v1.1 Final

---
+++ src/kata7/Kata7.java
@@ -0,0 +1,42 @@
+package kata7;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class Kata7 extends JFrame {
+
+    public static void main(String[] args) throws IOException {
+        new Kata7().launch();
+    }
+
+    public Kata7() throws IOException {
+        Watch watch = new Watch();
+        WatchDisplay watchDisplay = new WatchDisplay(background());
+        WatchPresenter watchPresenter = new WatchPresenter(watch, watchDisplay);
+        this.init();
+        this.add(watchDisplay);
+    }
+    
+    
+
+    private static Image background() throws IOException  {
+        return ImageIO.read(new File("background.jpg"));
+    }
+
+    private void launch() {
+        this.setVisible(true);
+    }
+
+    private void init() {
+        this.setTitle("Reloj");
+        this.setSize(400,400);
+        this.setResizable(false);
+        this.setUndecorated(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
+}
